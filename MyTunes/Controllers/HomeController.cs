22d126fb508d07649438e328eb421c0dc5988828
Djuan Ellis <djuan.ellis@gmail.com>
2018-06-30 13:36:32 -0400

    added music controller

---
+++ MyTunes/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using MyTunes.Models;
+using System.Web.Mvc;
+
+namespace MyTunes.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        
+            
+
+            
+        
+    }
+}
---
+++ MyTunes/Models/Music.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyTunes.Models
+{
+    public class Music
+    {
+        public string Album { get; set; }
+        
+    }
+}
