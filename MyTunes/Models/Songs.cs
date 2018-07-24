6cca52c48186d68f3dd7be027aa286d2b3424160
Djuan Ellis <djuan.ellis@gmail.com>
2018-07-24 19:29:53 -0400

    added required to songs

--- MyTunes/Models/Songs.cs
+++ MyTunes/Models/Songs.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,7 @@ namespace MyTunes.Models
     public class Songs
     {
         public int Id { get; set; }
+        [Required]
         public string SongTitle { get; set; }
         public string SongType { get; set; }
         public string SongVideo { get; set; }
